7dcbffde0608dcbfe9c2dd7f61583703893b9114
cmajerhua <carlos.majerhua@unmsm.edu.pe>
2018-06-13 19:05:57 -0700

    agregando conexion a mysql openshift

--- index.php
+++ index.php
@@ -1,7 +1,20 @@
 <?php
 
-	
 	echo "Hola mundo Open Shift";
+	$dbhost = getenv("MYSQL_SERVICE_HOST");
+	$dbport = getenv("MYSQL_SERVICE_PORT");
+	$dbuser = getenv("databaseuser");
+	$dbpwd = getenv("databasepassword");
+	$dbname = getenv("databasename");
 
+$con = mysqli_connect($dbhost,$dbuser,$dbpwd,$dbname);
+
+// Check connection
+if (mysqli_connect_errno())
+  {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}else{
+	echo "Conexion Correcta!";
+}
 
 ?>
